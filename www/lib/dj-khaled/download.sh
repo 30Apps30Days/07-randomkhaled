952db7d16fcece648da7eae197d4905bfcda2bf1
The Metaist <metaist@metaist.com>
2017-04-14 01:22:06 -0400

    add: more clips

--- www/lib/dj-khaled/download.sh
+++ www/lib/dj-khaled/download.sh
@@ -4,20 +4,20 @@ IFS=$'\n\t'
 
 
 dl() {
-  if [[ -e "$2.mp3" ]]; then return; fi
+  if [[ -e "$1.mp3" ]]; then return; fi
 
   youtube-dl -x --audio-format mp3 -w --no-part -o "%(id)s.%(ext)s" \
-    --postprocessor-args "$1" "https://youtu.be/$2"
+    "https://youtu.be/$1"
 }
 
 # All I Do Is Win
-dl "-ss 00:00:00.129 -t 00:00:02.6" QtPluXq_hko
+dl QtPluXq_hko
 
 # We Takin' Over
-dl "-ss 00:00:06 -t 00:00:02" HdT_oKderEs
+dl HdT_oKderEs
 
 # I'm on One
-dl "-ss 00:00:11 -t 00:00:02" zWhowrdR4dU
+dl zWhowrdR4dU
 
 # DJKhaledVEVO Channel
 youtube-dl -x --audio-format mp3 --no-part -o "%(id)s.%(ext)s" https://www.youtube.com/user/DJKhaledVEVO/video
